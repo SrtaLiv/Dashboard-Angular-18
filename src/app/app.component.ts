c6de8a95051e0eb1ffe2a0ab81ad5a0719338fe1
Olivia Todesco <104026140+SrtaLiv@users.noreply.github.com>
2025-04-03 13:25:40 -0300

    Configuracion de Dark mode, Light mode y estructura de carpetas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    MatSlideToggleModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Dashboard';
+
+  toggleDarkTheme(isDark: boolean) {
+    if (isDark) {
+      document.body.classList.add('dark-theme');
+    } else {
+      document.body.classList.remove('dark-theme');
+    }
+  }
 }
---
+++ src/app/components/molecules/input-label/input-label.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-label',
+  standalone: true,
+  imports: [],
+  templateUrl: './input-label.component.html',
+  styleUrl: './input-label.component.scss'
+})
+export class InputLabelComponent {
+
+}
